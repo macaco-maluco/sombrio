c62306229a7c079a083383eb1d23ba60d5bd69aa
Túlio Ornelas <tulio.ornelas@klarna.com>
2017-11-11 19:16:26 +0100

    Add basic php structure to deploy the static website to heroku

---
+++ scripts/deployWebsite.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+GITHUB_DOWNLOAD="https://github.com/Hackbit/nko2017-sombrio/releases/download"
+
+export MAC_DOWNLOAD_URL="${GITHUB_DOWNLOAD}/${TRAVIS_TAG}/Sombrio-darwin-x64.zip"
+export WINDOWS_DOWNLOAD_URL="${GITHUB_DOWNLOAD}/${TRAVIS_TAG}/Sombrio-linux-x64.zip"
+export LINUX_DOWNLOAD_URL="${GITHUB_DOWNLOAD}/${TRAVIS_TAG}/Sombrio-win32-x64.zip"
+
+pushd website && yarn install && yarn dist && popd
+
+rm -rf build-website
+mkdir build-website
+
+cp -r website/dist build-website/dist
+cp website/index.php build-website/index.php
+cp website/Composer.json build-website/Composer.json
+
+pushd build-website && zip -r website.zip ./ && popd
+mv build-website/website.zip .
+rm -rf build-website
