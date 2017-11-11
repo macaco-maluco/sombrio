26aab597e405db80190eb608cfaa056ab2dc9665
Paulo Ragonha <paulo@ragonha.me>
2017-11-11 21:17:58 +0100

    Very simple transitions 🚴

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -18,13 +18,23 @@ const styles = {
 const gridUnit = 60
 
 const Wall = ({ position }) => (
-  <g style={{ transform: `translate(${position[0]}px, ${position[1]}px)` }}>
+  <g
+    style={{
+      transition: 'transform 1s linear',
+      transform: `translate(${position[0]}px, ${position[1]}px)`,
+    }}
+  >
     <path d={`M0,0 ${gridUnit},0 ${gridUnit},${gridUnit} 0,${gridUnit}`} />
   </g>
 )
 
 const Target = ({ position }) => (
-  <g style={{ transform: `translate(${position[0]}px, ${position[1]}px)` }}>
+  <g
+    style={{
+      transition: 'transform 1s linear',
+      transform: `translate(${position[0]}px, ${position[1]}px)`,
+    }}
+  >
     <path
       stroke="blue"
       strokeWidth="4"
@@ -35,13 +45,23 @@ const Target = ({ position }) => (
 )
 
 const Player = ({ position }) => (
-  <g style={{ transform: `translate(${position[0]}px, ${position[1]}px)` }}>
+  <g
+    style={{
+      transition: 'transform 1s linear',
+      transform: `translate(${position[0]}px, ${position[1]}px)`,
+    }}
+  >
     <path fill="blue" d={`M0,0 ${gridUnit},0 ${gridUnit},${gridUnit} 0,${gridUnit}`} />
   </g>
 )
 
 const Monster = ({ position }) => (
-  <g style={{ transform: `translate(${position[0]}px, ${position[1]}px)` }}>
+  <g
+    style={{
+      transition: 'transform 1s linear',
+      transform: `translate(${position[0]}px, ${position[1]}px)`,
+    }}
+  >
     <path fill="red" d={`M0,0 ${gridUnit},0 ${gridUnit},${gridUnit} 0,${gridUnit}`} />
   </g>
 )
