8d871977450f2c093a4f54438145c2a54a495861
Túlio Ornelas <tulio.ornelas@klarna.com>
2017-11-12 22:40:24 +0100

    Update app and website title

--- client/src/index.html.js
+++ client/src/index.html.js
@@ -7,7 +7,7 @@ export default function({ htmlWebpackPlugin }) {
   <!DOCTYPE html>
   <html>
   <head>
-    <title>client</title>
+    <title>Sombrio</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <style>
